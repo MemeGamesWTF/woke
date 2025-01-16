1d85218b9bd790e38e045b9296175081a163d463
AaronHermit <116536507+AaronHermit@users.noreply.github.com>
2025-01-16 17:06:43 +0530

    new obstacel box

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-diddyOil-1.0";
+const cacheName = "DefaultCompany-woke-1.0";
 const contentToCache = [
     "Build/Build.loader.js",
     "Build/Build.framework.js",
